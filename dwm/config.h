64dd0f0848fb1ee7834208d4adcae2a34b66f256
madison <69394836+xorsirenz@users.noreply.github.com>
2025-02-02 18:16:24 -0700

    default tmux in ghostty

--- dwm/config.h
+++ dwm/config.h
@@ -66,13 +66,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_blue, "-nf", col_gray3, "-sb", col_blue, "-sf", col_peach, NULL };
 static const char *termcmd[]  = { "ghostty", NULL };
-static const char *termcmdTmux[]  = { "kitty", "-e", "tmux", NULL };
+static const char *termcmdTmux[]  = { "ghostty", "-e", "tmux", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmdTmux } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmdTmux } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
