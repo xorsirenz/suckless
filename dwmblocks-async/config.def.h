07b02d8d8051c93471de379094b665ca54c90d2f
madison <69394836+xorsirenz@users.noreply.github.com>
2025-02-19 21:23:31 -0700

    hmm

---
+++ dwmblocks-async/config.def.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// String used to delimit block outputs in the status.
+#define DELIMITER ""
+
+// Maximum number of Unicode characters that a block can output.
+#define MAX_BLOCK_OUTPUT_LENGTH 45
+
+// Control whether blocks are clickable.
+#define CLICKABLE_BLOCKS 1
+
+// Control whether a leading delimiter should be prepended to the status.
+#define LEADING_DELIMITER 0
+
+// Control whether a trailing delimiter should be appended to the status.
+#define TRAILING_DELIMITER 0
+
+// Define blocks for the status feed as X(icon, cmd, interval, signal).
+#define BLOCKS(X)             \
+    X("", "sb-packages", 5, 1) \
+    X("", "sb-vpn", 5, 2) \
+    X("", "sb-volume", 1, 3) \
+    X("", "sb-battery", 5, 4) \
+    X("", "date '+%H:%M'", 1, 5)
+ 
+#endif  // CONFIG_H
