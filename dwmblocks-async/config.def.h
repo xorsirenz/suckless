7e986ce7573811eec38f30ebcf8105bb3772f082
madison <69394836+xorsirenz@users.noreply.github.com>
2025-02-20 09:51:06 -0700

    space

--- dwmblocks-async/config.def.h
+++ dwmblocks-async/config.def.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER ""
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -23,5 +23,5 @@
     X("", "sb-volume", 1, 3) \
     X("", "sb-battery", 5, 4) \
     X("", "date '+%H:%M'", 1, 5)
- 
+
 #endif  // CONFIG_H
--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER ""
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -20,7 +20,7 @@
 #define BLOCKS(X)             \
     X("", "sb-packages", 5, 1) \
     X("", "sb-vpn", 5, 2) \
-    X("", "sb-volume", 0, 3) \
+    X("", "sb-volume", 1, 3) \
     X("", "sb-battery", 5, 4) \
     X("", "date '+%H:%M'", 1, 5)
 
